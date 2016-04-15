71c1a58f7fd40e73e6ce2dad5c9b6cc7e7f1de58
Jan-Erik Vinje <j.e.vinje@gmail.com>
2016-04-15 11:00:22 +0200

    Update Gruntfile.js
    Remove stuff not needed

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,38 +4,6 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         clean: ['build/'],
-        jsdoc: {
-            dist: {
-                src: ['lib/*.js'],
-                options: {
-                    destination: 'doc'
-                }
-            }
-        },
-        jshint: {
-            src: {
-                options: {
-                  jshintrc: '.jshintrc'
-                },
-                src: ['lib/*.js']
-            }
-        },
-        jasmine_node: {
-            options: {
-                forceExit: true,
-                match: '.',
-                matchall: false,
-                extensions: 'js',
-                specNameMatcher: 'spec',
-                jUnit: {
-                    report: true,
-                    savePath: "./build/reports/jasmine/",
-                    useDotNotation: true,
-                    consolidate: true
-                }
-            },
-            all: ['tests/unit/spec/']
-        },
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> v<%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -52,15 +20,9 @@ module.exports = function (grunt) {
     });
 
     // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-jsdoc');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-jasmine-node');
-    // uglify task is producing invalid js file
 
     // jasmine node directly js api 
-    grunt.registerTask('default', ['jshint', 'jasmine_node']);
-    grunt.registerTask('doc', ['jsdoc']);
     grunt.registerTask('minify', ['uglify']);
-    
+
 };
